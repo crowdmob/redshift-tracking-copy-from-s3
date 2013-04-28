d45352377f865e33d89b6a5382d07287328f7c41
Matthew Moore <matt@crowdmob.com>
2013-04-28 14:26:46 -0700

    config reading done

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
   "flag"
   "fmt"
   "os"
-  // configfile "github.com/crowdmob/goconfig"
+  "strings"
+  configfile "github.com/crowdmob/goconfig"
   // "github.com/crowdmob/goamz/aws"
   // "github.com/crowdmob/goamz/s3"
   // "github.com/crowdmob/goamz/exp/sns"
@@ -18,9 +19,9 @@ const (
   VERSION = "0.1"
 )
 
-var configFilename string
+var cfgFile string
 var shouldOutputVersion bool
-var configFile := struct {
+var cfg struct {
   Default struct {
     Debug                 bool
   }
@@ -52,21 +53,90 @@ var configFile := struct {
     Maxerror              int64
     Delimiter             string
   }
-  
 }
 
 func init() {
-  flag.StringVar(&configFilename, "c", "config/redshift-tracking-copy-from-s3.properties", "path to config file")
+  flag.StringVar(&cfgFile, "c", "config/redshift-tracking-copy-from-s3.properties", "path to config file")
 	flag.BoolVar(&shouldOutputVersion, "v", false, "output the current version and quit")
 }
 
-func reportError(errorMessage *string) {
+func reportError(desc string, err error) {
   // print to stderr and sns notify if required
+  if cfg.Sns.FailureNotifications && len(cfg.Sns.Topic) > 0 && len(cfg.Aws.Accesskey) > 0 && len(cfg.Aws.Secretkey) > 0 {
+    // TODO FIXME call SNS
+  }
+  fmt.Printf("%s: %s\n", desc, err)
+  panic(err)
 }
 
 func parseConfigfile() {
-  // use configFilename with config.*
-  // write into configFile and report errors
+  config, err := configfile.ReadConfigFile(cfgFile)
+  if err != nil {
+    fmt.Printf("Couldn't read config file %s because: %#v\n", cfgFile, err)
+    panic(err)
+  }
+  
+  // Prioritize config values required to send SNS notifications
+  cfg.Aws.Accesskey, err = config.GetString("aws", "accesskey")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Aws.Secretkey, err = config.GetString("aws", "secretkey")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Sns.FailureNotifications, err = config.GetBool("sns", "failure_notifications")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Sns.Topic, err = config.GetString("sns", "topic")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+
+  // Everything else
+  cfg.Default.Debug, err = config.GetBool("default", "debug")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Aws.Region, err = config.GetString("aws", "region")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+
+  var arrayString string
+  var stringsArray []string
+  
+  arrayString, err = config.GetString("s3", "buckets")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  stringsArray = strings.Split(arrayString, ",")
+  cfg.S3.Buckets = make([]string, len(stringsArray))
+  for i, _ := range stringsArray { cfg.S3.Buckets[i] = strings.TrimSpace(stringsArray[i]) }
+  
+  arrayString, err = config.GetString("s3", "prefixes")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  stringsArray = strings.Split(arrayString, ",")
+  cfg.S3.Prefixes = make([]string, len(stringsArray))
+  for i, _ := range stringsArray { cfg.S3.Prefixes[i] = strings.TrimSpace(stringsArray[i]) }
+  
+  arrayString, err = config.GetString("redshift", "tables")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  stringsArray = strings.Split(arrayString, ",")
+  cfg.Redshift.Tables = make([]string, len(stringsArray))
+  for i, _ := range stringsArray { cfg.Redshift.Tables[i] = strings.TrimSpace(stringsArray[i]) }
+  
+  cfg.Redshift.Migrate, err = config.GetBool("redshift", "migrate")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.SchemaJsonUrl, err = config.GetString("redshift", "schema_json_url")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Host, err = config.GetString("redshift", "host")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Port, err = config.GetInt64("redshift", "port")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Database, err = config.GetString("redshift", "database")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.User, err = config.GetString("redshift", "user")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Password, err = config.GetString("redshift", "password")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Emptyasnull, err = config.GetBool("redshift", "emptyasnull")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Blanksasnull, err = config.GetBool("redshift", "blanksasnull")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Fillrecord, err = config.GetBool("redshift", "fillrecord")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Maxerror, err = config.GetInt64("redshift", "maxerror")
+  if err != nil { reportError("Couldn't parse config: ", err) }
+  cfg.Redshift.Delimiter, err = config.GetString("redshift", "delimiter")
+  if err != nil { reportError("Couldn't parse config: ", err) }
 }
 
 func main() {
@@ -79,6 +149,8 @@ func main() {
   
   // Read config file
   parseConfigfile()
+  
+  fmt.Printf("Got redshift tables: %#v\n", cfg.Redshift.Tables)
   
   // Load schema json and check in redshift and migrate if needed
   // Startup goroutine for each Bucket/Prefix/Table
